091fb172378fa6aa24e6d68245cead21da8a0e59
nagasawa <x_nagasawa@flip.crap.jp>
2015-12-11 22:32:03 +0900

    * Update Jetty 8.1.15 to 9.3.6

--- src/main/java/com/github/kmkt/util/WSImageServlet.java
+++ src/main/java/com/github/kmkt/util/WSImageServlet.java
@@ -1,21 +1,39 @@
 package com.github.kmkt.util;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.eclipse.jetty.websocket.WebSocket;
-import org.eclipse.jetty.websocket.WebSocketServlet;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketListener;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
+import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WSImageServlet extends WebSocketServlet {
+    
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(WSImageServlet.class);
 
     private final Set<WebSocketCallback> connectedWebSockets = new CopyOnWriteArraySet<WebSocketCallback>();
+
+
+    @Override
+    public void configure(WebSocketServletFactory factory) {
+        logger.debug("configure");
+        factory.setCreator(new WebSocketCreator(){
+            @Override
+            public Object createWebSocket(ServletUpgradeRequest req,
+                    ServletUpgradeResponse resp) {
+                return new WebSocketCallback();
+            }
+        });
+    }
 
     /**
      * JPEG フレームデータを供給する。
@@ -38,44 +56,49 @@ public class WSImageServlet extends WebSocketServlet {
         }
     }
 
-    @Override
-    public WebSocket doWebSocketConnect(HttpServletRequest req, String prot) {
-        return new WebSocketCallback(req.getRemoteAddr());
-    }
-
-    private class WebSocketCallback implements WebSocket.OnTextMessage {
-        private Connection connection;
+    private class WebSocketCallback implements WebSocketListener {
+        private Session session;
         private String remoteAddr;
 
-        public WebSocketCallback(String remoteAddr) {
-            this.remoteAddr = remoteAddr;
+        @Override
+        public void onWebSocketConnect(Session session) {
+            this.session = session;
+            this.remoteAddr = session.getRemote().toString();
+
+            connectedWebSockets.add(this);
+            logger.debug("WebSocket connected from {}", remoteAddr);
         }
 
+
         @Override
-        public void onClose(int close_code, String msg) {
+        public void onWebSocketClose(int close_code, String msg) {
             connectedWebSockets.remove(this);
             logger.debug("WebSocket closed from {}", remoteAddr);
         }
 
         @Override
-        public void onOpen(Connection connection) {
-            this.connection = connection;
-            connectedWebSockets.add(this);
-            logger.debug("WebSocket connected from {}", remoteAddr);
+        public void onWebSocketError(Throwable error) {
+            logger.error("Error on WebSocket {} {}", session.getRemoteAddress(), error);
         }
 
+
         @Override
-        public void onMessage(String msg) {
+        public void onWebSocketText(String msg) {
             logger.debug("Message {} from {}", msg, remoteAddr);
         }
 
+        @Override
+        public void onWebSocketBinary(byte[] payload, int offset, int length) {
+            logger.debug("Message {} bytes from {}", length, remoteAddr);
+        }
+
         public boolean isOpen() {
-            return connection.isOpen();
+            return session.isOpen();
         }
 
         public void sendFrame(byte[] frame) {
             try {
-                connection.sendMessage(frame, 0, frame.length);
+                session.getRemote().sendBytes(ByteBuffer.wrap(frame));
             } catch (IOException e) {
                 logger.error(e.getMessage(), e);
             }
